c1422392099754c3eab3b885829be65501cf7e4d
Magicks52 <72513899+Magicks52@users.noreply.github.com>
2022-03-10 09:30:18 -0800

    added func check_status to new scripts

--- bash_scripts/secure_users.sh
+++ bash_scripts/secure_users.sh
@@ -10,3 +10,23 @@ BOLDRED="\e[1;${RED}m"
 ENDCOLOR="\e[0m"
 
 FAILED=$false
+
+#######################################
+# Checks the exit code of last command and reports success or failure
+# Globals:
+#     BOLDGREEN, BOLDRED, ENDCOLOR
+# Arguments:
+#     command name
+# Outputs:
+#     Writes output to stdout
+# Returns:
+#     0 if command exited successfully, 1 if failed.
+#######################################
+function check_status() {
+if [ "$?" -eq "0" ]; then
+    echo -e "${BOLDGREEN}SUCCESS: ${1} ${ENDCOLOR}}"
+else
+    echo -e "${BOLDRED}FAILED: ${1} ${ENDCOLOR}}"
+    return 1
+fi
+}
