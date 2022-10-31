0f5118c8fef77de9ac1cf208fb97b218626ec43e
Magicks52 <72513899+Magicks52@users.noreply.github.com>
2022-10-31 21:08:38 +0000

    Mon, Oct 31, 2022, 9:08 PM +00:00

--- linux/linbread.sh
+++ linux/linbread.sh
@@ -135,8 +135,8 @@ configure_network (){
 	sed -i "s/^.*net.ipv4.conf.all.rp_filter.*$/net.ipv4.conf.all.rp_filter=1/g" /etc/sysctl.conf
 	sed -i "s/^.*net.ipv4.conf.default.rp_filter.*$/net.ipv4.conf.default.rp_filter=1/g" /etc/sysctl.conf
 net.ipv4.icmp_echo_ignore_broadcasts = 1
-	# 
-	sed -i "s/^.*net.ipv4.tcp_syncookies.*$/net.ipv4.tcp_syncookies=1/g" /etc/sysctl.conf
+	# Ignore ICMP broadcast requests
+	sed -i "s/^.*net.ipv4.icmp_echo_ignore_broadcasts.*$/net.ipv4.icmp_echo_ignore_broadcasts=1/g" /etc/sysctl.conf
 
 	sysctl -p
 }
