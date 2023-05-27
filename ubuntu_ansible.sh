4d135bc9fe7fb5f6823ca221ff339d44261e0682
John Nguyen <72513899+GiuseNguyenJohn@users.noreply.github.com>
2023-05-26 17:49:42 -0700

    add stig guideline ubuntu_ansible.sh

--- ubuntu_ansible.sh
+++ ubuntu_ansible.sh
@@ -3,6 +3,7 @@
 
 sudo apt install -y git ansible
 git clone https://github.com/ansible-lockdown/UBUNTU22-CIS.git
+git clone https://github.com/ansible-lockdown/UBUNTU20-STIG.git
 sed -i "s/ubtu22cis_rule_1_9: true/ubtu22cis_rule_1_9: false/g" ./UBUNTU22-CIS/defaults/main.yml
 # already have inventory file
 # copy and paste command below
